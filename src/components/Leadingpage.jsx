e9b43011339b0f26f6bcbbb8febcac3a93edec49
XTeoNuclearXX <mateocasgo7@gmail.com>
2024-11-22 12:23:57 -0500

    se arregla lo visual al ingresar

--- src/components/Leadingpage.jsx
+++ src/components/Leadingpage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Header from "./header";
 import "./Leadingpage.css";
-export const Leadinpage= () =>{
+export const Leadinpage = () => {
 
 
-   return (
+  return (
     <>
-    <Header/>
-    <div className="page-container">
+      <Header />
+      <div className="page-container">
+      </div>
+      <div className="title-leanding">
         <h2>Welcome to Our Adventures</h2>
         <p>Here we can explore different parts around the world.</p>
-     </div>
-     </>
-   );
- };
+      </div>
+    </>
+  );
+};
